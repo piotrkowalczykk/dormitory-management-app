e1b344bbc148ce8add30fb9d73a4c842c3fbad7e
Piotr Kowalczyk <104227126+piotrkowalczykk@users.noreply.github.com>
2024-12-14 15:50:55 +0100

    feat: add JWT token handling and authentication error management

--- server/src/main/java/com/piotrkowalczykk/dormitory_management_app/security/config/SecurityConfig.java
+++ server/src/main/java/com/piotrkowalczykk/dormitory_management_app/security/config/SecurityConfig.java
@@ -7,21 +7,26 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private JWTAuthEntryPoint jwtAuthEntryPoint;
     private final CustomUserDetailsService customUserDetailsService;
 
-    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, JWTAuthEntryPoint jwtAuthEntryPoint) {
         this.customUserDetailsService = customUserDetailsService;
+        this.jwtAuthEntryPoint = jwtAuthEntryPoint;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.csrf(customizer -> customizer.disable());
+        httpSecurity.exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint));
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS).disable());
         httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/auth/register").permitAll().anyRequest().authenticated());
         httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
---
+++ server/src/main/java/com/piotrkowalczykk/dormitory_management_app/security/utils/JsonWebToken.java
@@ -0,0 +1,66 @@
+package com.piotrkowalczykk.dormitory_management_app.security.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+
+@Component
+public class JsonWebToken {
+
+    public static final long JWT_EXPIRATION = 86400000;
+    public static final String SECRET_KEY = "7cd7a66ce35f74dba14c7c35f819883410f9874a4da2ceabdfe165eebd0bfb46";
+
+    public String generateToken(Authentication authentication){
+        String email = authentication.getName();
+        Date currentDate = new Date();
+        Date expirationDate = new Date(currentDate.getTime() + JWT_EXPIRATION);
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(currentDate)
+                .expiration(expirationDate)
+                .signWith(key())
+                .compact();
+    }
+
+    public SecretKey key(){
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    public String getEmailFromToken(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(key()).build().parse(token);
+            return true;
+        } catch (Exception e){
+            throw new AuthenticationCredentialsNotFoundException("token expired or incorrect");
+        }
+
+    }
+
+
+
+}
