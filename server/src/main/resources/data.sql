093c0335f2f75b12fc8c5b8e8b613939b9208f21
Piotr Kowalczyk <104227126+piotrkowalczykk@users.noreply.github.com>
2025-01-02 22:00:09 +0100

    fix: remove unnecessary data.sql initialization on every build

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,9 +1,24 @@
-INSERT INTO roles (name) VALUES ('USER');
-INSERT INTO roles (name) VALUES ('ADMIN');
+INSERT INTO roles (name)
+SELECT 'USER'
+WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'USER');
+
+INSERT INTO roles (name)
+SELECT 'ADMIN'
+WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'ADMIN');
 
 INSERT INTO users (created_at, date_of_birth, email_verified, email_verification_code_expiry_date, password_reset_code_expiry_date, academy,
- email, email_verification_code, first_name, gender, last_name, password, password_reset_code)
-VALUES (null, null, true, null, null, null, 'admin@dormitory.com', null, 'admin', 'MALE', 'admin', '$2a$10$7pj6pB5KyynD20aQM5.sGeAq3eUKOxhvz04mTNfjaHeYYAZbeTWf6', null);
+                   email, email_verification_code, first_name, gender, last_name, password, password_reset_code)
+SELECT null, null, true, null, null, null, 'admin@dormitory.com', null, 'admin', 'MALE', 'admin',
+       '$2a$10$7pj6pB5KyynD20aQM5.sGeAq3eUKOxhvz04mTNfjaHeYYAZbeTWf6', null
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'admin@dormitory.com');
 
-INSERT INTO user_roles (user_id, role_id) VALUES ('1', '1');
-INSERT INTO user_roles (user_id, role_id) VALUES ('1', '2');
+INSERT INTO user_roles (user_id, role_id)
+SELECT users.id, roles.id
+FROM users, roles
+WHERE users.email = 'admin@dormitory.com'
+  AND (roles.name = 'USER' OR roles.name = 'ADMIN')
+  AND NOT EXISTS (
+      SELECT 1
+      FROM user_roles ur
+      WHERE ur.user_id = users.id AND ur.role_id = roles.id
+  );
