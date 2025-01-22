82eee2453158ae4f95183e12243a5002bd8a6322
Piotr Kowalczyk <104227126+piotrkowalczykk@users.noreply.github.com>
2025-01-22 17:28:55 +0100

    feat: add initial news page layout

--- client/src/feed/pages/Home/Home.tsx
+++ client/src/feed/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import classes from './Home.module.css'
 import { Nav } from '../../components/Nav/Nav'
 import { News } from '../../components/News/News'
+import { Footer } from '../../../authentication/components/Footer/Footer'
 export function Home(){
     return (
         <div className={classes.container}>
@@ -10,8 +11,24 @@ export function Home(){
                     <h1><span style={{color: 'red'}}>.</span>News</h1>
                 </div>
                 <News title='Test ' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum orci justo, rutrum vitae rhoncus non, fermentum vitae massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc risus sem, lobortis eu lacus eget, pharetra ornare mi. Morbi sit amet libero id nulla malesuada pellentesque nec vel nisl.'
+                image='https://bip.malopolska.pl/Download/get/id,2902285.json' data='2002.20.22'/><News title='Test ' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum orci justo, rutrum vitae rhoncus non, fermentum vitae massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc risus sem, lobortis eu lacus eget, pharetra ornare mi. Morbi sit amet libero id nulla malesuada pellentesque nec vel nisl.'
+                image='https://bip.malopolska.pl/Download/get/id,2902285.json' data='2002.20.22'/>
+                <News title='Test ' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum orci justo, rutrum vitae rhoncus non, fermentum vitae massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc risus sem, lobortis eu lacus eget, pharetra ornare mi. Morbi sit amet libero id nulla malesuada pellentesque nec vel nisl.'
+                image='https://bip.malopolska.pl/Download/get/id,2902285.json' data='2002.20.22'/>
+                <News title='Test ' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum orci justo, rutrum vitae rhoncus non, fermentum vitae massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc risus sem, lobortis eu lacus eget, pharetra ornare mi. Morbi sit amet libero id nulla malesuada pellentesque nec vel nisl.'
+                image='https://bip.malopolska.pl/Download/get/id,2902285.json' data='2002.20.22'/>
+                <News title='Test ' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum orci justo, rutrum vitae rhoncus non, fermentum vitae massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc risus sem, lobortis eu lacus eget, pharetra ornare mi. Morbi sit amet libero id nulla malesuada pellentesque nec vel nisl.'
+                image='https://bip.malopolska.pl/Download/get/id,2902285.json' data='2002.20.22'/>
+                <News title='Test ' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum orci justo, rutrum vitae rhoncus non, fermentum vitae massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc risus sem, lobortis eu lacus eget, pharetra ornare mi. Morbi sit amet libero id nulla malesuada pellentesque nec vel nisl.'
+                image='https://bip.malopolska.pl/Download/get/id,2902285.json' data='2002.20.22'/>
+                <News title='Test ' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum orci justo, rutrum vitae rhoncus non, fermentum vitae massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc risus sem, lobortis eu lacus eget, pharetra ornare mi. Morbi sit amet libero id nulla malesuada pellentesque nec vel nisl.'
+                image='https://bip.malopolska.pl/Download/get/id,2902285.json' data='2002.20.22'/>
+                <News title='Test ' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum orci justo, rutrum vitae rhoncus non, fermentum vitae massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc risus sem, lobortis eu lacus eget, pharetra ornare mi. Morbi sit amet libero id nulla malesuada pellentesque nec vel nisl.'
+                image='https://bip.malopolska.pl/Download/get/id,2902285.json' data='2002.20.22'/>
+                <News title='Test ' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum orci justo, rutrum vitae rhoncus non, fermentum vitae massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc risus sem, lobortis eu lacus eget, pharetra ornare mi. Morbi sit amet libero id nulla malesuada pellentesque nec vel nisl.'
                 image='https://bip.malopolska.pl/Download/get/id,2902285.json' data='2002.20.22'/>
             </div>
+            <Footer />
         </div>
     )
 }
